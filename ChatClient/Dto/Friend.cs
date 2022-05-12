102c652575e66c7c668ba49badca90bd173af0d3
GeorgeKarlinzer <georg11.01@mail.ru>
2022-05-12 11:57:29 +0200

    Update

---
+++ ChatClient/Dto/Friend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    internal class Friend
+    {
+        public int UserId_1 { get; set; }
+        public int UserId_2 { get; set; }
+    }
+}
