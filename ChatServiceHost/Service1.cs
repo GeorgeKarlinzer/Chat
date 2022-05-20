3c3e301406e22dd61b9115718ce5f49634ae4ef9
GeorgeKarlinzer <georg11.01@mail.ru>
2022-05-20 14:50:30 +0200

    Reclear

---
+++ ChatServiceHost/Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServiceHost
+{
+    public partial class Service1 : ServiceBase
+    {
+        ServiceHost host;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(Service))
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
